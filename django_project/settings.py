6cb69ee2599f9746dae0ce788a389e662c8a56ed
MjDahlaki <mjdahlaki@gmail.com>
2025-01-15 06:59:39 +0000

    ohcommit

--- django_project/settings.py
+++ django_project/settings.py
@@ -81,9 +81,7 @@ WSGI_APPLICATION = "django_project.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    "default": env.dj_db_url("DATABASE_URL", default=os.path.join(BASE_DIR, "db.sqlite3")),
-}
+DATABASES = { "default": env.dj_db_url("DATABASE_URL", default=f"sqlite:///{os.path.join(BASE_DIR, 'db.sqlite3')}"), }
 
 
 # Password validation
