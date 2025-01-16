c5eb05f61621592709a326a38330d60f8f29a18c
MjDahlaki <mjdahlaki@gmail.com>
2025-01-16 17:42:02 +0000

    commit to fix or debug errors

--- django_project/settings.py
+++ django_project/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-e8^#jz@%i%re&528rf5hvakhz^52b@v8k6!0p0si0y&se20-gj"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
