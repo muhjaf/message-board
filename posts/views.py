8b03971a1cf6f613d1766f6340d33d3d4d80b80b
MjDahlaki <mjdahlaki@gmail.com>
2025-01-17 21:14:35 +0000

    final commit

--- posts/views.py
+++ posts/views.py
@@ -5,3 +5,8 @@ class HomePageView(ListView):
     model = Post
     template_name = "home.html"
     context_object_name = 'post_list'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        print("Rendering template with context:", context)
+        return context
